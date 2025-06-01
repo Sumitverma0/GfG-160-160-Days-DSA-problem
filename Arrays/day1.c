8893e1f66cf93d308d506bbf824d9d0c980e30a4
Sumitverma0 <vermasumit5494@gmail.com>
2025-06-01 09:23:04 +0530

    DAY 01

---
+++ Arrays/day1.c
@@ -0,0 +1,15 @@
+int getSecondLargest(int *arr, int n) {
+    // code here
+    int max=-1;
+    int secmax=-1;
+    for (int i =0;i<n;i++){
+        if (arr[i]>max){
+            secmax=max;
+            max=arr[i];
+        }
+        else if(arr[i]<max && arr[i]>secmax){
+            secmax=arr[i];
+        }
+    }
+    return secmax;
+}
